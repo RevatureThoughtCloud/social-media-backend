4b2746baa673a4c8812ecc45a744f76ec20f9ed5
Elian <efelix.v@gmail.com>
2023-01-15 18:13:13 -0500

    Add cors for s3 bucket hosted frontend

--- src/main/java/com/revature/controllers/PostController.java
+++ src/main/java/com/revature/controllers/PostController.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/post")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://p3-dist.s3-website-us-east-1.amazonaws.com:4200"}, allowCredentials = "true")
 public class PostController {
 
 	private final PostService postService;
