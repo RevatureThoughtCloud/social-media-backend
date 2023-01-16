38dec0881dd6c6776153dd682e6187147e2a094e
Elian <efelix.v@gmail.com>
2023-01-15 20:16:38 -0500

    Fix cors address

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://p3-dist.s3-website-us-east-1.amazonaws.com:4200"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://p3-dist.s3-website-us-east-1.amazonaws.com"}, allowCredentials = "true")
 public class AuthController {
 
     private final AuthService authService;
