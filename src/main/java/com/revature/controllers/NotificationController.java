ee640162b7f9c5a7c84095a71850e566de8f66b3
DESKTOP-IASUES3\abe <barbozabraham@gmail.com>
2023-01-21 15:03:22 -0800

    more headers

--- src/main/java/com/revature/controllers/NotificationController.java
+++ src/main/java/com/revature/controllers/NotificationController.java
@@ -17,7 +17,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/notifications")
 @CrossOrigin(origins = { "http://localhost:4200", "http://localhost:3000",
-		"http://p3-dist.s3-website-us-east-1.amazonaws.com/" }, allowCredentials = "true", allowedHeaders = "*")
+		"http://p3-dist.s3-website-us-east-1.amazonaws.com/" }, allowCredentials = "true", allowedHeaders = "*", methods = {
+				RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.DELETE, RequestMethod.PUT })
 public class NotificationController {
 	@Autowired
 	private NotificationService nServe;
