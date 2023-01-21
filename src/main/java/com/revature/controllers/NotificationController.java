9e467a9c073d4baa6691dd9a9576d10462205547
DESKTOP-IASUES3\abe <barbozabraham@gmail.com>
2023-01-21 15:17:10 -0800

    removed methods

--- src/main/java/com/revature/controllers/NotificationController.java
+++ src/main/java/com/revature/controllers/NotificationController.java
@@ -17,8 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/notifications")
 @CrossOrigin(origins = { "http://localhost:4200", "http://localhost:3000",
-		"http://p3-dist.s3-website-us-east-1.amazonaws.com/" }, allowCredentials = "true", allowedHeaders = "*", methods = {
-				RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.DELETE, RequestMethod.PUT })
+		"http://p3-dist.s3-website-us-east-1.amazonaws.com/" }, allowCredentials = "true", allowedHeaders = "*")
 public class NotificationController {
 	@Autowired
 	private NotificationService nServe;
